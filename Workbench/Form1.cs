1b1ed591c022d8391e412c8840c0385f8e120573
Scott Lilly <scottlilly@scottlilly.com>
2017-02-07 18:15:32 -0600

    Added event version of publish/subscribe pattern
    Added event version of publish/subscribe pattern

---
+++ Workbench/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+using Engine.PublishSubscribePattern.PatternVersion.Models;
+
+namespace Workbench
+{
+    public partial class Form1 : Form
+    {
+        private readonly Player _currentPlayer = new Player {HitPoints = 10};
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _currentPlayer.PlayerKilled += PlayerKilled;
+        }
+
+        private void PlayerKilled(object sender, EventArgs eventArgs)
+        {
+            MessageBox.Show("Got here!");
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            _currentPlayer.HitPoints -= 3;
+        }
+    }
+}
